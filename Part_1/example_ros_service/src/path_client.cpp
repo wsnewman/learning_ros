afcc2118b61238baac09674e7d569c43062b831c
wnewman <wsn@case.edu>
2016-02-03 15:00:55 -0500

    service example w stdr

--- Part_1/example_ros_service/src/path_client.cpp
+++ Part_1/example_ros_service/src/path_client.cpp
@@ -10,11 +10,21 @@
 #include <geometry_msgs/PoseStamped.h>
 using namespace std;
 
+geometry_msgs::Quaternion convertPlanarPhi2Quaternion(double phi) {
+    geometry_msgs::Quaternion quaternion;
+    quaternion.x = 0.0;
+    quaternion.y = 0.0;
+    quaternion.z = sin(phi / 2.0);
+    quaternion.w = cos(phi / 2.0);
+    return quaternion;
+}
+
 int main(int argc, char **argv) {
     ros::init(argc, argv, "path_client");
     ros::NodeHandle n;
     ros::ServiceClient client = n.serviceClient<example_ros_service::PathSrv>("path_service");
-
+    geometry_msgs::Quaternion quat;
+    
     while (!client.exists()) {
       ROS_INFO("waiting for service...");
       ros::Duration(1.0).sleep();
@@ -25,7 +35,7 @@ int main(int argc, char **argv) {
     //create some path points...this should be done by some intelligent algorithm, but we'll hard-code it here
     geometry_msgs::PoseStamped pose_stamped;
     geometry_msgs::Pose pose;
-    pose.position.x = 0.0;
+    pose.position.x = 1.0; // say desired x-coord is 1
     pose.position.y = 0.0;
     pose.position.z = 0.0; // let's hope so!
     pose.orientation.x = 0.0; //always, for motion in horizontal plane
@@ -34,8 +44,16 @@ int main(int argc, char **argv) {
     pose.orientation.w = 1.0; //sum of squares of all components of unit quaternion is 1
     pose_stamped.pose = pose;
     path_srv.request.nav_path.poses.push_back(pose_stamped);
-    // just for heck of it, push on some redundant poses; really should make these more interesting
+    
+    // some more poses...
+    quat = convertPlanarPhi2Quaternion(1.57); // get a quaterion corresponding to this heading
+    pose_stamped.pose.orientation = quat;   
+    pose_stamped.pose.position.y=1.0; // say desired y-coord is 1.0
     path_srv.request.nav_path.poses.push_back(pose_stamped);
+    
+    quat = convertPlanarPhi2Quaternion(3.14);
+    pose_stamped.pose.orientation = quat;  
+    //desired position is not updated...just the desired heading  
     path_srv.request.nav_path.poses.push_back(pose_stamped);
     client.call(path_srv);
 
