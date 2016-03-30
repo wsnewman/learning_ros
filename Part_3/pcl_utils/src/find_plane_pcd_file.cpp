ee785b9bf23dfee3704c11551cd9398d2d8e9e51
wnewman <wsn@case.edu>
2016-03-30 11:34:11 -0400

    start find plane example

--- Part_3/pcl_utils/src/find_plane_pcd_file.cpp
+++ Part_3/pcl_utils/src/find_plane_pcd_file.cpp
@@ -1,93 +1,63 @@
-//example_process_pcd.cpp
-// example of opening a kinect snapshot pcd file and interpreting it
+//find_plane_pcd_file.cpp
+// prompts for a pcd file name, reads the file, and displays to rviz on topic "pcd"
+// can select a patch; then computes a plane containing that patch
 //wsn March 2016
 
+#include<ros/ros.h> //generic C++ stuff
+#include <stdlib.h>
+#include <math.h>
 
-#include <pcl_utils/pcl_utils.h>
-#include <pcl/visualization/cloud_viewer.h>
+#include <sensor_msgs/PointCloud2.h> //useful ROS message types
+#include <pcl_ros/point_cloud.h> //to convert between PCL a nd ROS
+#include <pcl/ros/conversions.h>
+
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+
 
 #include <pcl/common/common_headers.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/visualization/pcl_visualizer.h>
-#include <pcl/console/parse.h>
+#include <pcl-1.7/pcl/point_cloud.h>
+#include <pcl-1.7/pcl/PCLHeader.h>
+
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "process_pcd"); //node name
+    ros::init(argc, argv, "pcd_publisher"); //node name
     ros::NodeHandle nh; 
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr pclKinect_clr_ptr_(new pcl::PointCloud<pcl::PointXYZRGB>); //pointer for color version of pointcloud
+
+    cout<<"enter pcd file name: ";
     string fname;
+    cin>>fname;
+    
+    if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (fname, *pclKinect_clr_ptr_) == -1) //* load the file
+  {
+    ROS_ERROR ("Couldn't read file \n");
+    return (-1);
+  }
+  std::cout << "Loaded "
+            << pclKinect_clr_ptr_->width * pclKinect_clr_ptr_->height
+            << " data points from file "<<fname<<std::endl;
+
+
+   //let's publish the point cloud in a ROS-compatible message; here's a publisher:
+    ros::Publisher pubCloud = nh.advertise<sensor_msgs::PointCloud2> ("/pcd", 1);
+    sensor_msgs::PointCloud2 ros_cloud;  //here is the ROS-compatible message
+    pcl::toROSMsg(*pclKinect_clr_ptr_, ros_cloud); //convert from PCL to ROS type this way
+    ros_cloud.header.frame_id = "camera_depth_optical_frame";
+    
     ROS_INFO("instantiating a pclUtils object");
     PclUtils pclUtils(&nh);
-    /*
-    //load a named pcd file
-    cout<<"enter file name (in current directory): ";
-    cin>>fname;
-    //if (0!= pclUtils.read_clr_pcd_file(fname)) //this version for color
-    if (0!= pclUtils.read_pcd_file(fname)){  
-        ROS_ERROR("error loading file");
-    }
-    */
-   // ------------------------------------
-   // -----Create example point cloud-----
-   // ------------------------------------
-   pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr (new pcl::PointCloud<pcl::PointXYZ>);
-   pcl::PointCloud<pcl::PointXYZRGB>::Ptr point_cloud_ptr (new pcl::PointCloud<pcl::PointXYZRGB>);
-   std::cout << "Genarating example point clouds.\n\n";
-   // We're going to make an ellipse extruded along the z-axis. The colour for
-   // the XYZRGB cloud will gradually go from red to green to blue.
-   uint8_t r(255), g(15), b(15);
-   for (float z(-1.0); z <= 1.0; z += 0.05)
-   {
-     for (float angle(0.0); angle <= 360.0; angle += 5.0)
-     {
-       pcl::PointXYZ basic_point;
-       basic_point.x = 0.5 * cosf (pcl::deg2rad(angle));
-       basic_point.y = sinf (pcl::deg2rad(angle));
-       basic_point.z = z;
-       basic_cloud_ptr->points.push_back(basic_point);
- 
-       pcl::PointXYZRGB point;
-       point.x = basic_point.x;
-       point.y = basic_point.y;
-       point.z = basic_point.z;
-       uint32_t rgb = (static_cast<uint32_t>(r) << 16 |
-               static_cast<uint32_t>(g) << 8 | static_cast<uint32_t>(b));
-       point.rgb = *reinterpret_cast<float*>(&rgb);
-       point_cloud_ptr->points.push_back (point);
-     }
-     if (z < 0.0)
-     {
-       r -= 12;
-       g += 12;
-     }
-     else
-     {
-       g -= 12;
-       b += 12;
-     }
-   }
-   basic_cloud_ptr->width = (int) basic_cloud_ptr->points.size ();
-   basic_cloud_ptr->height = 1;
-   point_cloud_ptr->width = (int) point_cloud_ptr->points.size ();
-   point_cloud_ptr->height = 1;    
+    
+    //publish the ROS-type message on topic "/ellipse"; can view this in rviz
+    while (ros::ok()) {
 
-   pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
-   //pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>); //color
-   //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>); //no color
-   //pcl::io::loadPCDFile<PointXYZ>(fname.c_str(),*cloud);
-   
-   //pclUtils.get_kinect_points(cloud);
-   //cout<<"main: got cloud with "<<cloud->points.size()<<" points"<<endl;
-   cout<<"starting viewer..."<<endl;
-   
-   //viewer.showCloud (cloud);
-   //viewer.showCloud (basic_cloud_ptr); //point_cloud_ptr
-   viewer.showCloud (point_cloud_ptr);
-   while (!viewer.wasStopped ())
-   {
-   }   
+        pubCloud.publish(ros_cloud);
+        if ()
+        ros::spinOnce();
+        ros::Duration(0.1).sleep();
+    }
     return 0;
 }
     
