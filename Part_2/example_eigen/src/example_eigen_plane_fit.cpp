dfd2d67b0bc83ad8a5a1902b0a114caa61cb3db2
wnewman <wsn@case.edu>
2016-07-11 21:01:20 -0400

    add notes on commands for pt2

--- Part_2/example_eigen/src/example_eigen_plane_fit.cpp
+++ Part_2/example_eigen/src/example_eigen_plane_fit.cpp
@@ -16,7 +16,7 @@
 using namespace std;
 //using namespace Eigen; //if you get tired of typing Eigen:: everywhere, uncomment this.
                          // but I'll leave this as required, for now, to highlight when Eigen classes are being used
-    double g_noise_gain = 0.1; //0.1; //decide how much noise to add to points; start with 0.0, and should get precise results
+    double g_noise_gain = 0.1; //0.1; //0.1; //decide how much noise to add to points; start with 0.0, and should get precise results
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "example_eigen_plane_fit"); //node name
@@ -128,13 +128,11 @@ int main(int argc, char** argv) {
     cout<<endl<<endl;
     ROS_INFO("starting identification of plane from data: ");
     // first compute the centroid of the data:
-    /*
-    Eigen::Vector3d centroid;
     // here's a handy way to initialize data to all zeros; more variants exist
-    centroid = Eigen::MatrixXd::Zero(3,1); // see http://eigen.tuxfamily.org/dox/AsciiQuickReference.txt
-*/
+    // see http://eigen.tuxfamily.org/dox/AsciiQuickReference.txt
     Eigen::Vector3d centroid = Eigen::MatrixXd::Zero(3,1);
-          //add all the points together:
+    
+    //add all the points together:
     npts = points_mat.cols(); // number of points = number of columns in matrix; check the size
     cout<<"matrix has ncols = "<<npts<<endl;
     for (int ipt =0;ipt<npts;ipt++) {
