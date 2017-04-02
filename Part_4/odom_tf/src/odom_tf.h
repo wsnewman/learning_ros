3540e02414a0f99b5445f86a0204cb5167124cac
wnewman <wsn@case.edu>
2017-04-02 17:08:28 -0400

    odom_tf edits to move fncs to xform_utils

--- Part_4/odom_tf/src/odom_tf.h
+++ Part_4/odom_tf/src/odom_tf.h
@@ -25,6 +25,7 @@
 #include <tf/LinearMath/Vector3.h>
 #include <tf/LinearMath/QuadWord.h>
 #include <tf/transform_broadcaster.h>
+#include <xform_utils/xform_utils.h>
  
 const double UPDATE_RATE=50.0; // for a 50Hz update rate
 // define a class, including a constructor, member variables and member functions
@@ -32,12 +33,12 @@ class OdomTf
 {
 public:
     OdomTf(ros::NodeHandle* nodehandle); //"main" will need to instantiate a ROS nodehandle, then pass it to the constructor
-
-    double convertPlanarQuat2Phi(geometry_msgs::Quaternion quaternion);   
+    XformUtils xform_utils;
+    //double convertPlanarQuat2Phi(geometry_msgs::Quaternion quaternion);   
     tf::StampedTransform put_pose_in_transform(geometry_msgs::Pose pose);
-    tf::StampedTransform stamped_transform_inverse(tf::StampedTransform stf);
-    geometry_msgs::PoseStamped get_pose_from_transform(tf::StampedTransform tf);
-    geometry_msgs::PoseStamped get_pose_stamped_from_odom(nav_msgs::Odometry odom);
+    //tf::StampedTransform stamped_transform_inverse(tf::StampedTransform stf);
+    //geometry_msgs::PoseStamped get_pose_from_transform(tf::StampedTransform tf);
+    //geometry_msgs::PoseStamped get_pose_stamped_from_odom(nav_msgs::Odometry odom);
     
 
     tf::StampedTransform stfBaseLinkWrtOdom_; //base link w/rt odom frame; get this from tf; 
@@ -57,14 +58,7 @@ public:
     tf::StampedTransform stfDriftyOdomWrtMap_;
     
     tf::StampedTransform get_tfBaseLinkWrtDriftyOdom() { return stfBaseLinkWrtDriftyOdom_; } 
-    
-    bool multiply_stamped_tfs(tf::StampedTransform A_stf, tf::StampedTransform B_stf,tf::StampedTransform &C_stf);
 
-    void printStampedTf(tf::StampedTransform sTf);    
-    void printStampedPose(geometry_msgs::PoseStamped stPose);    
-    void printTf(tf::Transform tf);
-
-    tf::Transform get_tf_from_stamped_tf(tf::StampedTransform sTf);     
     //illustrates a tf_listener in a class; this is somewhat more complex than creating a tf_listener in main()
     tf::TransformListener* tfListener_;   
     tf::TransformBroadcaster br_;
--- Part_4/odom_tf/src/odom_tf_fncs.cpp
+++ Part_4/odom_tf/src/odom_tf_fncs.cpp
@@ -60,7 +60,7 @@ OdomTf::OdomTf(ros::NodeHandle* nodehandle) : nh_(*nodehandle) { // constructor
     stfAmclBaseLinkWrtMap_.stamp_ = ros::Time::now();           
 
     cout<<endl<<"init stfAmclBaseLinkWrtMap_"<<endl;
-    printStampedTf(stfAmclBaseLinkWrtMap_);    
+    xform_utils.printStampedTf(stfAmclBaseLinkWrtMap_);    
     
     //initialize an odometry frame coincident with map and base-link
     stfDriftyOdomWrtMap_ = stfAmclBaseLinkWrtMap_;
@@ -89,165 +89,6 @@ void OdomTf::initializeSubscribers() {
     // add more subscribers here, as needed
 }
 
-
-//member helper function to set up publishers;
-/*
-void OdomTf::initializePublishers()
-{
-    ROS_INFO("Initializing Publishers"); //nav_msgs::Odometry
-    hybrid_pose_publisher_ = nh_.advertise<nav_msgs::Odometry>("hybrid_robot_state", 1, true);
-    hybrid_yaw_publisher_ = nh_.advertise<std_msgs::Float64>("hybrid_robot_yaw", 1, true);
-}
- */
-
-//some conversion utilities:
-
-double OdomTf::convertPlanarQuat2Phi(geometry_msgs::Quaternion quaternion) {
-    double quat_z = quaternion.z;
-    double quat_w = quaternion.w;
-    double phi = 2.0 * atan2(quat_z, quat_w); // cheap conversion from quaternion to heading for planar motion
-    return phi;
-}
-
-//some conversion utilities:
-//getting a transform from a stamped transform is trickier than expected--there is not "get" fnc for transform
-
-tf::Transform OdomTf::get_tf_from_stamped_tf(tf::StampedTransform sTf) {
-    tf::Transform tf(sTf.getBasis(), sTf.getOrigin()); //construct a transform using elements of sTf
-    return tf;
-}
-
-//a transform describes the position and orientation of a frame w/rt a reference frame
-//a Pose is a position and orientation of a frame of interest w/rt a reference frame
-// can re-interpret a transform as a named pose using this example function
-// the PoseStamped also has a header, which includes naming the reference frame
-
-geometry_msgs::PoseStamped OdomTf::get_pose_from_transform(tf::StampedTransform tf) {
-    //clumsy conversions--points, vectors and quaternions are different data types in tf vs geometry_msgs
-    geometry_msgs::PoseStamped stPose;
-    geometry_msgs::Quaternion quat; //geometry_msgs object for quaternion
-    tf::Quaternion tfQuat; // tf library object for quaternion
-    tfQuat = tf.getRotation(); // member fnc to extract the quaternion from a transform
-    quat.x = tfQuat.x(); // copy the data from tf-style quaternion to geometry_msgs-style quaternion
-    quat.y = tfQuat.y();
-    quat.z = tfQuat.z();
-    quat.w = tfQuat.w();
-    stPose.pose.orientation = quat; //set the orientation of our PoseStamped object from result
-
-    // now do the same for the origin--equivalently, vector from parent to child frame 
-    tf::Vector3 tfVec; //tf-library type
-    geometry_msgs::Point pt; //equivalent geometry_msgs type
-    tfVec = tf.getOrigin(); // extract the vector from parent to child from transform
-    pt.x = tfVec.getX(); //copy the components into geometry_msgs type
-    pt.y = tfVec.getY();
-    pt.z = tfVec.getZ();
-    stPose.pose.position = pt; //and use this compatible type to set the position of the PoseStamped
-    stPose.header.frame_id = tf.frame_id_; //the pose is expressed w/rt this reference frame
-    stPose.header.stamp = tf.stamp_; // preserve the time stamp of the original transform
-    return stPose;
-}
-
-//a function to multiply two stamped transforms;  this function checks to make sure the
-// multiplication is logical, e.g.: T_B/A * T_C/B = T_C/A
-// returns false if the two frames are inconsistent as sequential transforms
-// returns true if consistent A_stf and B_stf transforms, and returns result of multiply in C_stf
-// The reference frame and child frame are populated in C_stf accordingly
-
-bool OdomTf::multiply_stamped_tfs(tf::StampedTransform A_stf,
-        tf::StampedTransform B_stf, tf::StampedTransform &C_stf) {
-
-    //long-winded approach:
-    //std::string str1(A_stf.child_frame_id_); //want to compare strings to check consistency
-    //std::string str2(B_stf.frame_id_);
-    //if (str1.compare(str2) != 0) { //SHOULD get that child frame of A is parent frame of B
-    //more compact approach:
-    if (A_stf.child_frame_id_.compare(B_stf.frame_id_) != 0) {    
-        std::cout << "can't multiply transforms; mismatched frames" << endl;
-	std::cout << A_stf.child_frame_id_ << " is not " << B_stf.frame_id_ << '\n';
-        return false;
-    }
-    //if here, the named frames are logically consistent
-        tf::Transform C = A_stf*B_stf; //multiplication is defined for transforms
-	C_stf.setData(C);
-	C_stf.frame_id_ = A_stf.frame_id_;
-	C_stf.child_frame_id_ = B_stf.child_frame_id_;
-	C_stf.stamp_ = ros::Time::now();
- 
-    //long-winded approach, equivalent to above:
-    /*
-    tf::Transform A, B; //simple transforms--not stamped
-        
-    A = get_tf_from_stamped_tf(A_stf); // get the transform from the stamped transform
-    B = get_tf_from_stamped_tf(B_stf);
-    C = A*B; //multiplication is defined for transforms 
-    C_stf.frame_id_ = A_stf.frame_id_; //assign appropriate parent and child frames to result
-    C_stf.child_frame_id_ = B_stf.child_frame_id_;
-    C_stf.setOrigin(C.getOrigin()); //populate the origin and orientation of the result
-    C_stf.setBasis(C.getBasis());
-    C_stf.stamp_ = ros::Time::now(); //assign the time stamp to current time; 
-     * */
-    // alternatively, could assign this to the OLDER of A or B transforms
-    return true; //if got here, the multiplication is valid
-}
-
-tf::StampedTransform OdomTf::stamped_transform_inverse(tf::StampedTransform stf) {
-    // instantiate stamped transform with constructor args
-    //note: child_frame and frame_id are reversed, to correspond to inverted transform
-    tf::StampedTransform stf_inv(stf.inverse(), stf.stamp_, stf.child_frame_id_, stf.frame_id_);
-    /* long-winded equivalent:
-    tf::StampedTransform stf_inv;    
-    tf::Transform tf = get_tf_from_stamped_tf(stf);
-    tf::Transform tf_inv = tf.inverse();
-    
-    stf_inv.stamp_ = stf.stamp_;
-    stf_inv.frame_id_ = stf.child_frame_id_;
-    stf_inv.child_frame_id_ = stf.frame_id_;
-    stf_inv.setOrigin(tf_inv.getOrigin());
-    stf_inv.setBasis(tf_inv.getBasis());
-     * */
-    return stf_inv;
-}
-
-//fnc to print components of a transform
-
-void OdomTf::printTf(tf::Transform tf) {
-    tf::Vector3 tfVec;
-    tf::Matrix3x3 tfR;
-    tf::Quaternion quat;
-    tfVec = tf.getOrigin();
-    cout << "vector from reference frame to child frame: " << tfVec.getX() << "," << tfVec.getY() << "," << tfVec.getZ() << endl;
-    tfR = tf.getBasis();
-    cout << "orientation of child frame w/rt reference frame: " << endl;
-    tfVec = tfR.getRow(0);
-    cout << tfVec.getX() << "," << tfVec.getY() << "," << tfVec.getZ() << endl;
-    tfVec = tfR.getRow(1);
-    cout << tfVec.getX() << "," << tfVec.getY() << "," << tfVec.getZ() << endl;
-    tfVec = tfR.getRow(2);
-    cout << tfVec.getX() << "," << tfVec.getY() << "," << tfVec.getZ() << endl;
-    quat = tf.getRotation();
-    cout << "quaternion: " << quat.x() << ", " << quat.y() << ", "
-            << quat.z() << ", " << quat.w() << endl;
-}
-
-//fnc to print components of a stamped transform
-
-void OdomTf::printStampedTf(tf::StampedTransform sTf) {
-    tf::Transform tf;
-    cout << "frame_id: " << sTf.frame_id_ << endl;
-    cout << "child_frame_id: " << sTf.child_frame_id_ << endl;
-    tf = get_tf_from_stamped_tf(sTf); //extract the tf from the stamped tf  
-    printTf(tf); //and print its components      
-}
-
-//fnc to print components of a stamped pose
-
-void OdomTf::printStampedPose(geometry_msgs::PoseStamped stPose) {
-    cout << "frame id = " << stPose.header.frame_id << endl;
-    cout << "origin: " << stPose.pose.position.x << ", " << stPose.pose.position.y << ", " << stPose.pose.position.z << endl;
-    cout << "quaternion: " << stPose.pose.orientation.x << ", " << stPose.pose.orientation.y << ", "
-            << stPose.pose.orientation.z << ", " << stPose.pose.orientation.w << endl;
-}
-
 void OdomTf::odomCallback(const nav_msgs::Odometry& odom_rcvd) {
     odom_count_++;
     // copy some of the components of the received message into member vars
@@ -262,7 +103,7 @@ void OdomTf::odomCallback(const nav_msgs::Odometry& odom_rcvd) {
     odom_y_ = odom_rcvd.pose.pose.position.y;
     odom_quat_ = odom_rcvd.pose.pose.orientation;
     //odom publishes orientation as a quaternion.  Convert this to a simple heading
-    odom_phi_ = convertPlanarQuat2Phi(odom_quat_); // cheap conversion from quaternion to heading for planar motion
+    odom_phi_ = xform_utils.convertPlanarQuat2Phi(odom_quat_); // cheap conversion from quaternion to heading for planar motion
     //get the position from odom, converted to a tf type
     tf::Vector3 pos;
     pos.setX(odom_pose_.position.x);
@@ -294,7 +135,7 @@ void OdomTf::odomCallback(const nav_msgs::Odometry& odom_rcvd) {
     //here's a trick: publish the drifty_odom frame as a child of the robot's base_frame to avoid a kinematic loop
     //rviz will then be able to transform the drifty_odom frame appropriately in whatever fixed frame is chosen for display
     // use stamped_transform_inverse() function to invert base w/rt odom into odom w/rt base
-    stfDriftyOdomWrtBase_ = stamped_transform_inverse(stfBaseLinkWrtDriftyOdom_); 
+    stfDriftyOdomWrtBase_ = xform_utils.stamped_transform_inverse(stfBaseLinkWrtDriftyOdom_); 
     //use the TRANSFORM PUBLISHER to send out this transform on the tf topic
     br_.sendTransform(stfDriftyOdomWrtBase_);
     
@@ -303,22 +144,22 @@ void OdomTf::odomCallback(const nav_msgs::Odometry& odom_rcvd) {
     // this transform will update infrequently (e.g. 1Hz), but it also should only change slowly (at the rate of odom drift)
 
     //cout<<endl<<"stfDriftyOdomWrtMap_"<<endl;
-    //printStampedTf(stfDriftyOdomWrtMap_);     
+    //xform_utils.printStampedTf(stfDriftyOdomWrtMap_);     
     // cout<<endl<<"stfBaseLinkWrtDriftyOdom_"<<endl;
-    //printStampedTf(stfBaseLinkWrtDriftyOdom_);       
+    //xform_utils.printStampedTf(stfBaseLinkWrtDriftyOdom_);       
     
     //cascde the frames: for frames b==base_link, m==map, do==drifty_odom,
     // 
     // T_b/m = m^T_b = m^T_do * do^T_b = T_do/m * T_b/do
     // put the answer in stfEstBaseWrtMap_
-    multiply_stamped_tfs(stfDriftyOdomWrtMap_,stfBaseLinkWrtDriftyOdom_,stfEstBaseWrtMap_);
+    xform_utils.multiply_stamped_tfs(stfDriftyOdomWrtMap_,stfBaseLinkWrtDriftyOdom_,stfEstBaseWrtMap_);
     //publish this frame, for visualization; change "base_link" to "est_base" to avoid name conflict
     // and kinematic loop;  Visualized result of est_base is an estimate of the robot's pose in
     // the map frame, using amcl and using the imperfect (drifty) odom estimate
     // ideally, this frame is virtually perfect, matching the base_link frame known to rviz
     stfEstBaseWrtMap_.child_frame_id_ = "est_base";
     //cout<<endl<<"stfEstBaseWrtMap_"<<endl;
-    // printStampedTf(stfEstBaseWrtMap_);   
+    // xform_utils.printStampedTf(stfEstBaseWrtMap_);   
     //publish this transform, making it available to rviz, known as "est_base"
     br_.sendTransform(stfEstBaseWrtMap_);
       
@@ -335,7 +176,7 @@ void OdomTf::odomCallback(const nav_msgs::Odometry& odom_rcvd) {
 //  continue to use the same drifty_odom w/rt map transform until we get a new one  (which should be pretty close to the previous one)
 void OdomTf::amclCallback(const geometry_msgs::PoseWithCovarianceStamped& amcl_rcvd) {
     amcl_pose_ = amcl_rcvd.pose.pose;
-    ROS_WARN("amcl pose: x, y, yaw: %f, %f, %f", amcl_pose_.position.x, amcl_pose_.position.y, convertPlanarQuat2Phi(amcl_pose_.orientation));
+    ROS_WARN("amcl pose: x, y, yaw: %f, %f, %f", amcl_pose_.position.x, amcl_pose_.position.y, xform_utils.convertPlanarQuat2Phi(amcl_pose_.orientation));
     //stfAmclBaseLinkWrtMap_
     amcl_quat_ = amcl_pose_.orientation;
     tf::Vector3 pos;
@@ -354,13 +195,13 @@ void OdomTf::amclCallback(const geometry_msgs::PoseWithCovarianceStamped& amcl_r
     stfAmclBaseLinkWrtMap_.frame_id_ = "map";
     stfAmclBaseLinkWrtMap_.child_frame_id_ = "base_link";
     //cout<<endl<<"stfAmclBaseLinkWrtMap_"<<endl;
-    //printStampedTf(stfAmclBaseLinkWrtMap_);      
+    //xform_utils.printStampedTf(stfAmclBaseLinkWrtMap_);      
     
-    stfDriftyOdomWrtBase_ = stamped_transform_inverse(stfBaseLinkWrtDriftyOdom_); 
+    stfDriftyOdomWrtBase_ = xform_utils.stamped_transform_inverse(stfBaseLinkWrtDriftyOdom_); 
     //cout<<endl<<"stfDriftyOdomWrtBase_"<<endl;
-    //printStampedTf(stfDriftyOdomWrtBase_);    
+    //xform_utils.printStampedTf(stfDriftyOdomWrtBase_);    
     
-    multiply_stamped_tfs(stfAmclBaseLinkWrtMap_,stfDriftyOdomWrtBase_,stfDriftyOdomWrtMap_);
+    xform_utils.multiply_stamped_tfs(stfAmclBaseLinkWrtMap_,stfDriftyOdomWrtBase_,stfDriftyOdomWrtMap_);
     
    //publish the estimated odom frame w/rt the map.  This is useful for visualization, e.g. to see
     //how rapidly the odometry estimate is drifting
@@ -371,7 +212,7 @@ void OdomTf::amclCallback(const geometry_msgs::PoseWithCovarianceStamped& amcl_r
     // THIS TRANSFORM will only get updated when new amcl estimates are available...e.g. 1Hz
     stfDriftyOdomWrtMap_.child_frame_id_ = "corrected_odom";
     //cout<<endl<<"stfDriftyOdomWrtMap_"<<endl;
-    //printStampedTf(stfDriftyOdomWrtMap_);      
+    //xform_utils.printStampedTf(stfDriftyOdomWrtMap_);      
     br_.sendTransform(stfDriftyOdomWrtMap_);   
     //done publishing, so restore correct frame name
     stfDriftyOdomWrtMap_.child_frame_id_ = "drifty_odom"; //change this frame name back, for use
